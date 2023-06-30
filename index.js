e90c64e2db012a176c25aa67261bd999854ff1dc
Fuji belajar <131544178+Fujirenshuu@users.noreply.github.com>
2023-06-30 14:52:25 +0700

    feat: Add error handling
    This commit adds a feature for error handling and provides informative feedback to users. It includes the implementation of try-catch blocks to ensure application stability.

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const apiKey = "YOUR_API_KEY";
-function weather(lokasi) {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      lokasi +
+function weather(location) {
+  try {
+    const url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      location +
       "&appid=" +
       apiKey +
-      "units=metric"
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      console.log("Location :", response.name);
-      console.log("Country :", response.sys.country);
-      console.log("°С", response.main.temp);
-      console.log(
-        "-----------",
-        response.weather[0].description,
-        "-----------"
-      );
-      console.log(
-        "min",
-        response.main.temp_min,
-        "°С",
-        "max",
-        response.main.temp_max,
-        "°С"
-      );
-      console.log("Cordinat :", response.wind.speed), "m/s";
-      console.log("Clouds", response.clouds.all, "%");
-      console.log("Geo Coordinates : ", response.coord.lat, response.coord.lon);
-    });
+      "&units=metric";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Location:", data.name);
+        console.log("Country:", data.sys.country);
+        console.log("Temperature:", data.main.temp + "°С");
+        console.log("-----------", data.weather[0].description, "-----------");
+        console.log(
+          "Min:",
+          data.main.temp_min,
+          "°С",
+          "Max:",
+          data.main.temp_max,
+          "°С"
+        );
+        console.log("Wind Speed:", data.wind.speed, "m/s");
+        console.log("Clouds:", data.clouds.all, "%");
+        console.log(
+          "Geo Coordinates:",
+          data.coord.lat,
+          data.coord.lon
+        );
+      });
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
+
